8c1bc4e779a2845c5b13dda0760ae8f5b9c19ddc
codingtothecore <codingtothecore@skiff.com>
2024-01-24 14:11:34 +0530

    first commit

---
+++ components/Input.tsx
@@ -0,0 +1,45 @@
+interface InputProps {
+    placeholder?: string;
+    value?: string;
+    disabled?: boolean;
+    type?: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+
+const Input: React.FC<InputProps> = ({
+    placeholder,
+    value,
+    disabled,
+    type,
+    onChange
+}) => {
+    return ( 
+        <input 
+            disabled={disabled}
+            onChange={onChange}
+            placeholder={placeholder}
+            value={value}
+            type={type}
+            className={`
+                w-full
+                p-4
+                text-lg
+                bg-black
+                border-2
+                border-neutral-800
+                rounded-md
+                outline-none
+                text-white
+                focus:border-sky-500
+                focus:border-2
+                transition
+                disabled:bg-neutral-900
+                disabled:opacity-70
+                disabled:cursed-not-allowed
+            `}
+        />
+     );
+}
+ 
+export default Input;
